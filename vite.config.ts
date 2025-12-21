bc6da181b515ac2964de02292fc9fb6dea9a7373
JoJoGong <ugong@uw.edu>
2025-12-21 23:08:50 +0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      },
-      base: '/CREATIVE-TECHNOLOGIST-PORTFOLIO/',
+      }
     };
 });
 
